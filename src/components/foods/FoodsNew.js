90b38d99b3932f40693c93c5e1430795d14f8a24
AndyLegacy <andystav91@googlemail.com>
2017-11-03 10:45:01 +0000

    refactored food search

--- src/components/foods/FoodsNew.js
+++ src/components/foods/FoodsNew.js
@@ -8,16 +8,37 @@ import '../../scss/components/foods/FoodsNew.scss';
 class FoodsNew extends React.Component {
   state = {
     food: {},
-    date: ''
+    date: '',
+    search: '',
+    meal: '',
+    foods: []
   }
 
-  handleChange = ({ target: { value }}) => {
-    this.setState({ date: value });
+  handleChange = ({ target: { name, value }}) => {
+    this.setState({ [name]: value });
+  }
+
+  handleMealChange = ({ target: { value }}) => {
+    this.setState(prevState => {
+      const food = { ...prevState.food, meal: value };
+      return { food };
+    });
   }
 
   addFood = chosenFood => {
-    // receive food from search
-    this.setState({ food: chosenFood });
+    this.setState(prevState => {
+      const food = { ...prevState.food, ...chosenFood };
+      return { food, search: '', foods: [] };
+    });
+  }
+
+  handleFoodSearch = e => {
+    e.preventDefault();
+    // make Axios request to /api/fatsecret
+    Axios
+      .get(`/api/fatsecret?search=${this.state.search}`)
+      .then(res => this.setState({ foods: res.data }))
+      .catch(err => console.log(err));
   }
 
   handleSubmit = e => {
@@ -32,21 +53,41 @@ class FoodsNew extends React.Component {
 
   render() {
     const { food } = this.state;
-    console.log(food);
+    console.log(this.state);
     return (
       <section className="FoodsNew">
-        <div className="row">
-          <div className="col-md-2">
+        <div className="row foods-search">
+          <form onSubmit={this.handleFoodSearch}>
             <input className="date"
               type="date"
               name="date"
               placeholder="YYYY-MM-DD"
               onChange={this.handleChange}
               value={this.state.date} />
-          </div>
-          <div className="col-md-6">
-            <FoodsSearch addFood={this.addFood} />
-          </div>
+
+            <input className="foodBar"
+              name="search"
+              type="search"
+              onChange={this.handleChange}
+              value={this.state.search}
+            />
+            <select
+              className="btn btn-lg btn-primary mealDrop"
+              name="meal"
+              onChange={this.handleMealChange}
+              value={this.state.food.meal}>
+              <option>Meal Time</option>
+              <option>Breakfast</option>
+              <option>Lunch</option>
+              <option>Dinner</option>
+              <option>Snack</option>
+            </select>
+            <button className="btn btn-primary searchFood">Search Foods</button>
+          </form>
+        </div>
+
+        <div className="row foods-list">
+          <FoodsSearch addFood={this.addFood} foods={this.state.foods} />
         </div>
 
 
--- src/components/foods/FoodsSearch.js
+++ src/components/foods/FoodsSearch.js
@@ -1,64 +1,14 @@
 import React from 'react';
-import Axios from 'axios';
 import '../../scss/components/foods/FoodsSearch.scss';
 class FoodsSearch extends React.Component {
-
-  state = {
-    meal: '',
-    foods: [],
-    search: ''
-  }
-
-  handleChange = ({ target: { value }}) => {
-    this.setState({ search: value });
-  }
-  handleMeal = ({ target: { value }}) => {
-    this.setState({ meal: value });
-
-  }
-  handleSubmit = e => {
-    e.preventDefault();
-    // make Axios request to /api/fatsecret
-    Axios
-      .get(`/api/fatsecret?search=${this.state.search}`)
-      .then(res => this.setState({ foods: res.data}))
-      .catch(err => console.log(err));
-  }
-
-  addFood = (food) => {
-    const newFood = Object.assign(food, { meal: this.state.meal });
-    this.props.addFood(newFood);
-    this.setState({ foods: [], search: '' });
-  }
-
   render() {
-    console.log(this.state.foods);
+    console.log(this.props.foods);
     return (
       <div className="row all">
-        <div className="col-md-12">
-          <form onSubmit={this.handleSubmit}>
-            <input className="foodBar"
-              type="search"
-              onChange={this.handleChange}
-              value={this.state.search}
-            />
-            <select
-              className="btn btn-lg btn-primary mealDrop"
-              onChange={this.handleMeal}
-              value={this.state.meal}>
-              <option>Meal Time</option>
-              <option>Breakfast</option>
-              <option>Lunch</option>
-              <option>Dinner</option>
-              <option>Snack</option>
-            </select>
-            <button className="btn btn-primary searchFood">Search Foods</button>
-          </form>
-        </div>
         <div className="row">
-          {this.state.foods && this.state.foods.map((food, i) => {
+          {this.props.foods && this.props.foods.map((food, i) => {
             return (
-              <div key={i} onClick={() => this.addFood(food)} className="col-md-3 col-sm-6 days-wrapper">
+              <div key={i} onClick={() => this.props.addFood(food)} className="col-md-4 col-sm-6 days-wrapper">
                 <div className="result">
                   <div>
                     <h3 >{food.meal}</h3>
